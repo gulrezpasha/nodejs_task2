3988613fe031bd494cb430c77ff561e6e64cc79d
gulrezpasha <gulrezturk1853@gmail.com>
2026-01-12 16:19:51 +0530

    done

--- app.js
+++ app.js
@@ -1,51 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        fs.readFile('user.txt', (err, data) => {
-            if (err) data = '';
-
-            res.setHeader('Content-Type', 'text/html');
-            res.end(`
-                
-                <p>${data.toString().replace(/\n/g, '<br>')}</p>
-
-                <form method="POST" action="/submit">
-                    <label>Name:</label>
-                    <input type="text" name="username" required />
-                    <button type="submit">Send</button>
-                </form>
-            `);
-        });
-    }
-
-    if (url === '/submit' && method === 'POST') {
-        let body = [];
-        req.on('data', chunk => {
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const buffer = Buffer.concat(body).toString();
-            const formvalues = buffer.split('=')[1];
-
-            
-            fs.readFile('user.txt', 'utf8', (err, oldData) => {
-                if (err) oldData = '';
-                const newData = formvalues + '\n';
-
-                fs.writeFile('user.txt', newData, err => {
-                    res.statusCode = 302;
-                    res.setHeader('Location', '/');
-                    res.end();
-                });
-            });
-        });
-    }
-});
+const routes=require('./routes')
+routes.anothertext();
+const server = http.createServer(routes);
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ routes.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const requestHandler=(req,res)=>{
+     const url = req.url;
+        const method = req.method;
+    
+        if (url === '/') {
+            fs.readFile('user.txt', (err, data) => {
+                if (err) data = '';
+    
+                res.setHeader('Content-Type', 'text/html');
+                res.end(`
+                    
+                    <p>${data.toString().replace(/\n/g, '<br>')}</p>
+    
+                    <form method="POST" action="/submit">
+                        <label>Name:</label>
+                        <input type="text" name="username" required />
+                        <button type="submit">Send</button>
+                    </form>
+                `);
+            });
+        }
+    
+        if (url === '/submit' && method === 'POST') {
+            let body = [];
+            req.on('data', chunk => {
+                body.push(chunk);
+            });
+            req.on('end', () => {
+                const buffer = Buffer.concat(body).toString();
+                const formvalues = buffer.split('=')[1];
+    
+                
+                fs.readFile('user.txt', 'utf8', (err, oldData) => {
+                    if (err) oldData = '';
+                    const newData = formvalues + '\n';
+    
+                    fs.writeFile('user.txt', newData, err => {
+                        res.statusCode = 302;
+                        res.setHeader('Location', '/');
+                        res.end();
+                    });
+                });
+            });
+        }
+}
+const anothertext=function(){
+    console.log("this is another function")
+}
+module.exports={
+    requestHandler,
+    anothertext
+}
+// module.exports={
+//     requestHandler:requestHandler,
+//     text:anotherfunction
+// }
