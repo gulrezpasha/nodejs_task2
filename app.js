5b90b8e603e705e452e04f0202bd39ede43f923f
touseefraza <touseefraza29@gmail.com>
2026-01-19 01:37:30 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const port=3000;
+const addUser=(req,res,next)=>{
+    req.user="Guest",
+    next();
+}
+app.get('/welcome',addUser,(req,res)=>{
+    res.send(`<h1>Welcome ${req.user}</h1>`);
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
