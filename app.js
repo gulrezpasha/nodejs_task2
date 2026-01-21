f508831d2b87026e63653ba283586d26f2ad7e3e
touseefraza <touseefraza29@gmail.com>
2026-01-22 01:12:20 +0530

    using unknown routes

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express=require('express');
 const app=express();
-const port=3000;
+const port=4000;
+
+app.use(express.json());
 app.get('/',(req,res)=>{
     console.log("request recieved at root url");
+     res.send('<h1>Welcome to Home Page</h1>');
 })
 
-app.get('/orders',(req,res)=>{
-    res.send(`<h1>"Here is the list of all orders."</h1>`);
+app.get('/products',(req,res)=>{
+    res.send(`<h1>"Here is the list of all products."</h1>`);
 })
-app.post('/orders',(req,res)=>{
-    res.send(`<h1>"A new order has been created."</h1>`);
+app.post('/products',(req,res)=>{
+    res.send(`<h1>"A new product has been added."</h1>`);
 })
-app.get('/users',(req,res)=>{
-    res.send(`<h1>"Here is the list of all users."</h1>`);
+app.get('/categories',(req,res)=>{
+    res.send(`<h1>"Here is the list of all categories."</h1>`);
 })
-app.post('/users',(req,res)=>{
-    res.send(`<h1>"A new user has been added."</h1>`);
+app.post('/categories',(req,res)=>{
+    res.send(`<h1>"A new category has been created."</h1>`);
 })
+
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
