9e57ff03f662ad29526d63cf4fef826564a5d873
gulrezpasha <gulrezturk1853@gmail.com>
2026-01-28 01:28:20 +0530

    using router

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-
-
 const express = require('express');
 const app = express();
+const productsCategoriesRouter = require('./routes/productsCategories');
+
+app.use(express.json());
+
+
 app.use((req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next();
-});
-app.get('/products', (req, res) => {
-    res.send('Here is the list of all products.');
-});
-app.post('/products', (req, res) => {
-    res.send('A new product has been added.');
-});
-app.get('/categories', (req, res) => {
-    res.send('Here is the list of all categories.');
-});
-app.post('/categories', (req, res) => {
-    res.send('A new category has been created.');
+  console.log(`${req.method} request made to ${req.url}`);
+  next();
 });
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+app.use('/', productsCategoriesRouter);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
