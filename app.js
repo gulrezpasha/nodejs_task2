6de848e8f07f37a8691e32c7169cd92d444d3a57
gulrezpasha <gulrezturk1853@gmail.com>
2026-02-03 00:13:40 +0530

    use of controller

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const express=require('express');
+const express= require('express');
 const app=express();
 const port=3000;
-app.use(express.json());
-const homeroute=require('./routes/Home');
-const courseroute=require('./routes/course');
-const studentsroute=require('./routes/Students');
-app.use('/',homeroute);
-app.use('/courses',courseroute);
-app.use('/course/:id',courseroute)
-app.use('/students',studentsroute);
-
-app.use((req,res,next)=>{
-  res.status(404).send('page not found');
-})
+const productRoutes = require('./routes/productRoutes');
+app.use('/products', productRoutes);
 app.listen(port,()=>{
   console.log(`server is running on port ${port}`);
 })
---
+++ controller/productcontroller.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const { get } = require('http');
+
+
+const getAllProducts = (req, res) => {
+    res.send("Fetching all products");
+};
+
+const getProductById = (req, res) => {
+    const { id } = req.params;
+    res.send(`Fetching product with ID: ${id}`);
+};
+
+const addProduct = (req, res) => {
+    res.send("Adding a new product");
+};
+
+module.exports = {
+    getAllProducts,
+    getProductById,
+    addProduct
+};
